8986cf05a3d70fc2f53d12d7c712580851a80644
Emanuel Ferreira <contatoferreirads@gmail.com>
2023-05-30 11:22:16 -0300

    chore: refactor

--- src/analyzeLinks.ts
+++ src/analyzeLinks.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 import config from '../config.json';
 
 async function hasAvailableWebsites() {
@@ -9,18 +9,10 @@ async function hasAvailableWebsites() {
     return true;
 }
 
-
-(async () => {
-    if(!hasAvailableWebsites()) {
-        console.warn('No websites to analyze');
-        return;
-    }
-
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    
-    // Navigate to the website to analyze
-    await page.goto('https://example.com');
+async function getEssayLinks(page: Page) {
+    await page.goto('http://paulgraham.com/articles.html', {
+        timeout: 0,
+    });
     
     // Get all <a> tags on the page
     const links = await page.$$eval('a', (anchors) =>
@@ -28,7 +20,25 @@ async function hasAvailableWebsites() {
         href: a.href,
         text: a.innerText.trim(),
         }))
-    );
+    );    
+
+    return links;
+}
+
+
+(async () => {
+    if(!hasAvailableWebsites()) {
+        console.warn('No websites to analyze');
+        return;
+    }
+
+    const browser = await puppeteer.launch({
+        headless: "new",
+    });
+
+    const page = await browser.newPage();
+
+    const links = await getEssayLinks(page);
 
     // Display the essay links
     console.log('Essay Links:');
