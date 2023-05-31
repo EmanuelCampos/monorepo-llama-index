56df3bab9521ce8f0c4ac37a77cbc09a5ad58ac8
Emanuel Ferreira <contatoferreirads@gmail.com>
2023-05-31 18:40:09 -0300

    chore: rename some files (#7)

--- packages/api/src/api.py
+++ packages/api/src/api.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from llama_index import StorageContext, load_index_from_storage
+from llama_index import StorageContext, load_index_from_storage, ServiceContext, LLMPredictor
+from langchain.llms import OpenAI
 
 # rebuild storage context
-storage_context = StorageContext.from_defaults(persist_dir="./storage")
+storage_context = StorageContext.from_defaults(persist_dir="storage")
 
-# load index
-index = load_index_from_storage(storage_context)
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -16,7 +16,15 @@ cors = CORS(app)
 def hello_world():
     body = request.get_json()
 
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=LLMPredictor(llm=OpenAI(model_name='text-davinci-003', temperature=0, openai_api_key=body['apiKey']))
+    )
+
+    global index
+    index = load_index_from_storage(storage_context, service_context=service_context)
+
     query_engine = index.as_query_engine()
+
     response = query_engine.query(body['question'])
     
     return jsonify({ "answer": response.response })
